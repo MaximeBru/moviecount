6d0cea015bce967fa98ebd280000bf603280802a
hajidev <haji75013@gmail.com>
2016-07-19 14:45:42 +0200

    mise à jour 19-07

--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -3,20 +3,53 @@
 namespace Controller;
 
 use \W\Controller\Controller;
+use \W\Manager\UserManager;
+use \Manager\MyAuthentificationManager;
 
 class LoginController extends Controller
 {
 
+	public function inscription()
+	{
+		if (isset($_POST['inscrire'])) {
+			// on instancie un objet pour gérer l'accès à la table "wusers"
+			$manager = new UserManager();
+			$_POST['myform']['role'] = 'user';
+			$_POST['myform']['password'] = password_hash($_POST['myform']['password'], PASSWORD_DEFAULT);
+			$manager->insert($_POST["myform"]);
+			$this->redirectToRoute('home');
+		}
+
+		$this->show('login/inscription');
+	}
+
 
 	public function connexion()
 	{
+		if (isset($_POST['connexion'])) {
+			// pour réaliser la connexion d'un utilisateur, nous avons besoin de deux "managers" :
+			// un pour l'authentification et l'autre pour récupérer les informations de l'utilisateur (role, username, email, id)
+			$authManager = new MyAuthentificationManager();
+			$userManager = new UserManager();
+
+			// On teste via le manager de l'authentification si les informations entrées par l'utilisateur sont valides
+			if($authManager->isValidLoginInfo($_POST['myform']['email'], $_POST['myform']['password'])) {
+
+				// je récupère les infos de l'utilisateur
+				$user = $userManager->getUserByUsernameOrEmail($_POST['myform']['email']);
+
+				// on logue l'utilisateur
+				$authManager->LogUserIn($user);
+
+				// on redirige vers son profil
+				$this->redirectToRoute('home');
+			}
+		}
+
 		$this->show('login/connexion');
+
 	}
 
-	public function inscription()
-	{
-		$this->show('login/inscription');
-	}
 
 	public function deconnexion()
 	{
--- app/Manager/FilmManager.php
+++ app/Manager/FilmManager.php
@@ -12,12 +12,46 @@ class FilmManager extends \W\Manager\Manager {
 		return $sth->fetchAll();
 	}
 
-	public function getTopFilm($limit = 5)
+	public function getTopFilm($limit = 5)	//Top 5 films les mieux notés
 	{
-		$sql = "SELECT id_film_api, id_film, avg(vote_count)as moyenne FROM users_stats as u_s LEFT JOIN " . $this->table . " as f ON u_s.id_film = f.id group by id_film order by moyenne desc limit ". $limit;
+		$sql = "SELECT id_film_api, id_film, titre, img, avg(vote_count)as moyenne FROM users_stats as u_s LEFT JOIN " . $this->table . " as f ON u_s.id_film = f.id WHERE u_s.stat_view = 1 group by id_film order by moyenne desc limit ". $limit;
 		$sth = $this->dbh->prepare($sql);
 		$sth->execute();
 		return $sth->fetchAll();
 	}
+
+	public function topListeVu($limit = 5) 	//Top 5  des films ajoutés à la liste vu (stat_view = 1)
+	{
+		$sql = "SELECT id_film_api, id_film, titre, img, COUNT(*)as topAjout FROM users_stats as u_s LEFT JOIN " . $this->table . " as f ON u_s.id_film = f.id WHERE u_s.stat_view = 1 group by id_film order by topAjout desc limit " . $limit;
+		$sth = $this->dbh->prepare($sql);
+		$sth->execute();
+		return $sth->fetchAll();
+	}
+
+	public function topListeVeutvoir($limit = 5)	//Top 5 des films ajoutés à la liste "veut voir" (stat_view = 2)
+	{
+		$sql = "SELECT id_film_api, id_film, COUNT(*)as topAjout FROM users_stats as u_s LEFT JOIN " . $this->table . " as f ON u_s.id_film = f.id WHERE u_s.stat_view = 2 group by id_film order by topAjout desc limit" . $limit;
+		$sth = $this->dbh->prepare($sql);
+		$sth->execute();
+		return $sth->fetchAll();
+	}
+
+	public function topUsers($limit = 6)	//Top 6 des personnes qui ont vu le plus de films
+	{
+		$sql = "SELECT username, id_user, COUNT(*)as topUser FROM users_stats as u_s LEFT JOIN wusers as u ON u_s.id_user = u.id WHERE u_s.stat_view = 1 group by id_user order by topUser desc limit $limit";
+		$sth = $this->dbh->prepare($sql);
+		$sth->execute();
+		return $sth->fetchAll();
+
+	}
+
+	public function lastActivity($limit = 6)	//Dernières activités de la communauté
+	{
+		$sql = "SELECT username, id_user, date_ajout, stat_view FROM users_stats as u_s LEFT JOIN wusers as u ON u_s.id_user = u.id order by date_ajout desc limit $limit";
+		$sth = $this->dbh->prepare($sql);
+		$sth->execute();
+		return $sth->fetchAll();
+	}
+	
 
 }
