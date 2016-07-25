fe212972cd8cf96aef14ebdcb600d29e4110531b
hajidev <haji75013@gmail.com>
2016-07-25 12:31:13 +0200

    maj class btn

--- app/templates/default/detail.php
+++ app/templates/default/detail.php
@@ -46,12 +46,12 @@
 
 			<!-- BTN  -->
 			<?php if(isset($_SESSION['user'])) { ?>
-				<div id="btn">
+				<div class="btn">
 					<a href="<?= $this->url('jveuxvoir', ['id_user' => $_SESSION['user']['id'], 'id_film' => $film['id']])?>" title="j'veux voir !"><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 					<a href="<?= $this->url('dejavu', ['id_user' => $_SESSION['user']['id'], 'id_film' => $film['id']])?>" title="j'ai déjà vu""><i class="fa fa-eye" aria-hidden="true"></i></a>
 				</div>
 			<?php } else { ?>
-				<div id="btn">
+				<div class="btn">
 					<a><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 					<a><i class="fa fa-eye" aria-hidden="true"></i></a>
 				</div>
@@ -78,12 +78,12 @@
 					<h3><?= mb_strimwidth($film['titre'], 0, 23, "...") ?></h3>
 				</a>
 				<?php if(isset($_SESSION['user'])) { ?>
-					<div id="btn">
+					<div class="btn">
 						<a href="<?= $this->url('dejavu', ['id_user' => $_SESSION['user']['id'], 'id_film' => $film['id']])?>" title="j'ai déjà vu !""><i class="fa fa-eye" aria-hidden="true"></i></a>
 						<a href="<?= $this->url('jveuxvoir', ['id_user' => $_SESSION['user']['id'], 'id_film' => $film['id']])?>" title="j'veux voir !"><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 					</div>
 				<?php } else { ?>
-					<div id="btn">
+					<div class="btn">
 						<a><i class="fa fa-eye" aria-hidden="true"></i></a>
 						<a><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 					</div>
--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -1,12 +1,15 @@
 <?php $this->layout('layout', ['title' => 'Accueil']) ?>
 
-<?php $this->start('main_content') ?>
+<!-- <?php $this->start('main_content') ?>
 	<?php if(isset($_SESSION['message'])) { ?>
 		<div><?= $_SESSION['message'] ?></div>
 	<?php } 
 		unset($_SESSION['message']);
-	?>
+	?> -->
 
+<div id="message" style="display: none;background:red;color:white;">
+CONNEXION REQUISE
+</div>
 
 <div id="home">
 	<h1><span>MovieCount</span>, Votre profil cinéma en ligne.</h1>
@@ -22,12 +25,12 @@
 				<h3><?= mb_strimwidth($film['titre'], 0, 23, "...") ?></h3>
 			</a>
 			<?php if(isset($_SESSION['user'])) { ?>
-				<div id="btn">
+				<div class="btn">
 					<a href="<?= $this->url('dejavu', ['id_user' => $_SESSION['user']['id'], 'id_film' => $film['id_film']])?>" title="j'ai déjà vu !"><i class="fa fa-eye" aria-hidden="true"></i></a>
 					<a href="<?= $this->url('jveuxvoir', ['id_user' => $_SESSION['user']['id'], 'id_film' => $film['id_film']])?>" title="j'veux voir !"><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 				</div>
 			<?php } else { ?>
-				<div id="btn">
+				<div class="btn">
 					<a><i class="fa fa-eye" aria-hidden="true"></i></a>
 					<a><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 				</div>
@@ -63,12 +66,12 @@
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
 				<?php if(isset($_SESSION['user'])) { ?>
-					<div id="btn">
+					<div class="btn">
 						<a href="#" title="j'ai déjà vu !"><i class="fa fa-eye" aria-hidden="true"></i></a>
 						<a href="#" title="j'veux voir !"><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 					</div>
 				<?php } else { ?>
-					<div id="btn">
+					<div class="btn">
 						<a><i class="fa fa-eye" aria-hidden="true"></i></a>
 						<a><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 					</div>
@@ -113,12 +116,12 @@
 				<h3><?= mb_strimwidth($film['titre'], 0, 23, "...") ?></h3>
 			</a>
 			<?php if(isset($_SESSION['user'])) { ?>
-				<div id="btn">
+				<div class="btn">
 					<a href="<?= $this->url('dejavu', ['id_user' => $_SESSION['user']['id'], 'id_film' => $film['id_film']])?>" title="j'ai déjà vu""><i class="fa fa-eye" aria-hidden="true"></i></a>
 					<a href="<?= $this->url('jveuxvoir', ['id_user' => $_SESSION['user']['id'], 'id_film' => $film['id_film']])?>" title="j'veux voir !"><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 				</div>
 			<?php } else { ?>
-				<div id="btn">
+				<div class="btn">
 					<a><i class="fa fa-eye" aria-hidden="true"></i></a>
 					<a><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 				</div>
@@ -159,12 +162,12 @@
 					<h3><?= mb_strimwidth($film['titre'], 0, 23, "...") ?></h3>
 				</a>
 				<?php if(isset($_SESSION['user'])) { ?>
-					<div id="btn">
+					<div class="btn">
 						<a href="<?= $this->url('dejavu', ['id_user' => $_SESSION['user']['id'], 'id_film' => $film['id']])?>" title="j'ai déjà vu !""><i class="fa fa-eye" aria-hidden="true"></i></a>
 						<a href="<?= $this->url('jveuxvoir', ['id_user' => $_SESSION['user']['id'], 'id_film' => $film['id']])?>" title="j'veux voir !"><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 					</div>
 				<?php } else { ?>
-					<div id="btn">
+					<div class="btn">
 						<a><i class="fa fa-eye" aria-hidden="true"></i></a>
 						<a><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
 					</div>
@@ -176,3 +179,14 @@
 
 
 <?php $this->stop('main_content') ?>
+
+<?php $this->start('javascripts') ?>
+<?php if(!isset($_SESSION['user'])) { ?>
+<script>
+	$('.btn').on('click', function() {
+		$('#message').slideUp( 300 ).show().delay( 3000 ).fadeOut( 500 );
+	})
+</script>
+<?php } ?>
+
+<?php $this->stop('javascripts') ?>
